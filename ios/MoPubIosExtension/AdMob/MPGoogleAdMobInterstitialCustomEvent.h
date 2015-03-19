1269f3dfe1335dea45ee57712132c27124683b80
Toinouze <antoinekleinpeter@Antoines-Mac.local>
2015-03-19 01:41:09 -0700

    iOS
    AdMob
        - admob is now integrated on iOS

---
+++ ios/MoPubIosExtension/AdMob/MPGoogleAdMobInterstitialCustomEvent.h
@@ -0,0 +1,21 @@
+//
+//  MPGoogleAdMobInterstitialCustomEvent.h
+//  MoPub
+//
+//  Copyright (c) 2012 MoPub, Inc. All rights reserved.
+//
+
+#if __has_include(<MoPub/MoPub.h>)
+#import <MoPub/MoPub.h>
+#else
+#import "MPInterstitialCustomEvent.h"
+#endif
+
+/*
+ * MoPub certified with version 6.12.2 (Jun. 6, 2014) of the Google AdMob Ads SDK.
+ * Pixmix certified with version 7.0.0 (Feb. 3, 2015) of the Google AdMob Ads SDK.
+ */
+
+@interface MPGoogleAdMobInterstitialCustomEvent : MPInterstitialCustomEvent <GADInterstitialDelegate>
+
+@end
---
+++ ios/MoPubIosExtension/AdMob/MPGoogleAdMobInterstitialCustomEvent.m
@@ -0,0 +1,133 @@
+//
+//  MPGoogleAdMobInterstitialCustomEvent.m
+//  MoPub
+//
+//  Copyright (c) 2012 MoPub, Inc. All rights reserved.
+//
+
+#import <GoogleMobileAds/GADInterstitial.h>
+#import "MPGoogleAdMobInterstitialCustomEvent.h"
+#import "MPInterstitialAdController.h"
+#import "MPLogging.h"
+#import "MPAdConfiguration.h"
+#import "MPInstanceProvider.h"
+#import <CoreLocation/CoreLocation.h>
+
+@interface MPInstanceProvider (AdMobInterstitials)
+
+- (GADInterstitial *)buildGADInterstitialAd;
+- (GADRequest *)buildGADInterstitialRequest;
+
+@end
+
+@implementation MPInstanceProvider (AdMobInterstitials)
+
+- (GADInterstitial *)buildGADInterstitialAd
+{
+    return [[GADInterstitial alloc] init];
+}
+
+- (GADRequest *)buildGADInterstitialRequest
+{
+    return [GADRequest request];
+}
+
+@end
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+@interface MPGoogleAdMobInterstitialCustomEvent ()
+
+@property (nonatomic, strong) GADInterstitial *interstitial;
+
+@end
+
+@implementation MPGoogleAdMobInterstitialCustomEvent
+
+@synthesize interstitial = _interstitial;
+
+#pragma mark - MPInterstitialCustomEvent Subclass Methods
+
+- (void)requestInterstitialWithCustomEventInfo:(NSDictionary *)info
+{
+    MPLogInfo(@"Requesting Google AdMob interstitial");
+    
+    NSString* adUnitID = [info objectForKey:@"adUnitID"];
+    
+    if (adUnitID == nil) {
+        MPLogError(@"No ad unit ID specified for AdMob, cannot load interstitial !");
+        [self.delegate interstitialCustomEvent:self didFailToLoadAdWithError:nil];
+        return;
+    }
+    
+    MPLogInfo(@"Using AdMob ad unit ID : %@", adUnitID);
+    
+    self.interstitial = [[MPInstanceProvider sharedProvider] buildGADInterstitialAd];
+    
+    self.interstitial.adUnitID = [info objectForKey:@"adUnitID"];
+    self.interstitial.delegate = self;
+    
+    GADRequest *request = [[MPInstanceProvider sharedProvider] buildGADInterstitialRequest];
+    request.requestAgent = @"Mopub";
+    
+    CLLocation *location = self.delegate.location;
+    if (location) {
+        [request setLocationWithLatitude:location.coordinate.latitude
+                               longitude:location.coordinate.longitude
+                                accuracy:location.horizontalAccuracy];
+    }
+    
+    [self.interstitial loadRequest:request];
+}
+
+- (void)showInterstitialFromRootViewController:(UIViewController *)rootViewController
+{
+    [self.interstitial presentFromRootViewController:rootViewController];
+}
+
+- (void)dealloc
+{
+    self.interstitial.delegate = nil;
+    [super dealloc];
+}
+
+#pragma mark - GADInterstitialDelegate
+
+- (void)interstitialDidReceiveAd:(GADInterstitial *)interstitial
+{
+    MPLogInfo(@"Google AdMob Interstitial did load");
+    [self.delegate interstitialCustomEvent:self didLoadAd:self];
+}
+
+- (void)interstitial:(GADInterstitial *)interstitial didFailToReceiveAdWithError:(GADRequestError *)error
+{
+    MPLogInfo(@"Google AdMob Interstitial failed to load with error: %@", error.localizedDescription);
+    [self.delegate interstitialCustomEvent:self didFailToLoadAdWithError:error];
+}
+
+- (void)interstitialWillPresentScreen:(GADInterstitial *)interstitial
+{
+    MPLogInfo(@"Google AdMob Interstitial will present");
+    [self.delegate interstitialCustomEventWillAppear:self];
+    [self.delegate interstitialCustomEventDidAppear:self];
+}
+
+- (void)interstitialWillDismissScreen:(GADInterstitial *)ad
+{
+    MPLogInfo(@"Google AdMob Interstitial will dismiss");
+    [self.delegate interstitialCustomEventWillDisappear:self];
+}
+
+- (void)interstitialDidDismissScreen:(GADInterstitial *)ad
+{
+    MPLogInfo(@"Google AdMob Interstitial did dismiss");
+    [self.delegate interstitialCustomEventDidDisappear:self];
+}
+
+- (void)interstitialWillLeaveApplication:(GADInterstitial *)ad
+{
+    MPLogInfo(@"Google AdMob Interstitial will leave application");
+    [self.delegate interstitialCustomEventDidReceiveTapEvent:self];
+}
+
+@end
