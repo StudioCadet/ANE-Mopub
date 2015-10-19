e1eaee933e9258abe47f13234d4cd262d9c4c0c8
Maxime Philipp <maxime@studiopixmix.com>
2015-10-19 17:07:44 +0200

    Revert "Smart ad server : added the location info on banners and interstitials"
    This reverts commit ff51ba46ced8551612c30016af9e055782895a5d.

--- android/src/com/mopub/mobileads/SmartAdBanner.java
+++ android/src/com/mopub/mobileads/SmartAdBanner.java
@@ -3,21 +3,16 @@ package com.mopub.mobileads;
 import java.util.Map;
 
 import android.content.Context;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Bundle;
 import android.view.View;
 
 import com.smartadserver.android.library.SASBannerView;
 import com.smartadserver.android.library.model.SASAdElement;
 import com.smartadserver.android.library.ui.SASAdView;
-import com.smartadserver.android.library.ui.SASAdView.OnStateChangeListener;
 import com.smartadserver.android.library.ui.SASAdView.StateChangeEvent;
 import com.sticksports.nativeExtensions.mopub.MoPubExtension;
 
 /**
- * Adapter for Smart Ad Server banners. 
+ * Adapter for Smart Ad Server banners.
  * Tested with Smart Ad Server 6.1
  */
 public class SmartAdBanner extends CustomEventBanner implements View.OnClickListener, SASAdView.AdResponseHandler, SASAdView.OnStateChangeListener {
@@ -27,8 +22,7 @@ public class SmartAdBanner extends CustomEventBanner implements View.OnClickList
 	private CustomEventBannerListener listener;
 	/** The banner view. */
 	private SASBannerView banner;
-	/** The state listener used for the location. */
-	private OnStateChangeListener stateChangeListener;
+
 
 	//////////////////////////
 	// CUSTOM EVENT METHODS //
@@ -36,6 +30,7 @@ public class SmartAdBanner extends CustomEventBanner implements View.OnClickList
 
 	@Override
 	protected void loadBanner(Context context, CustomEventBannerListener listener, Map<String, Object> localExtras, Map<String, String> serverExtras) {
+
 		this.listener = listener;
 
 		MoPubExtension.log("Creating a SAS banner ...");
@@ -47,8 +42,9 @@ public class SmartAdBanner extends CustomEventBanner implements View.OnClickList
 		Integer formatId = SmartAdUtils.getFormatIdFromServerExtras(serverExtras);
 		String target = SmartAdUtils.getTargetFromServerExtras(serverExtras);
 
-		if (siteId == null || pageId == null || formatId == null || target == null) {
-			MoPubExtension.logE("Invalid SmartAdServer parameters ! Configure network on MoPub to provide custom data : {\"siteId\":YOUR_SITE_ID, \"pageId\":YOUR_PAGE_ID, \"formatId\":YOUR_FORMAT_ID, \"target\":YOUR_TARGET}");
+		if(siteId == null || pageId == null || formatId == null || target == null) {
+			MoPubExtension.logE("Invalid SmartAdServer parameters ! Configure network on MoPub to provide custom data : "
+					+ "{\"siteId\":YOUR_SITE_ID, \"pageId\":YOUR_PAGE_ID, \"formatId\":YOUR_FORMAT_ID, \"target\":YOUR_TARGET}");
 			listener.onBannerFailed(MoPubErrorCode.ADAPTER_CONFIGURATION_ERROR);
 			return;
 		}
@@ -63,10 +59,9 @@ public class SmartAdBanner extends CustomEventBanner implements View.OnClickList
 	protected void onInvalidate() {
 		MoPubExtension.log("Invalidating SAS banner ...");
 		listener = null;
-		if (banner != null) {
+		if(banner != null) {
 			MoPubExtension.log("Destroying SAS banner ...");
 			banner.removeStateChangeListener(this);
-			banner.removeStateChangeListener(stateChangeListener);
 			banner.setOnClickListener(null);
 			banner.onDestroy();
 		}
@@ -74,21 +69,21 @@ public class SmartAdBanner extends CustomEventBanner implements View.OnClickList
 		MoPubExtension.log("SAS banner invalidated.");
 	}
 
-	// ///////////////////////////
+
+	/////////////////////////////
 	// SAS AD RESPONSE METHODS //
-	// ///////////////////////////
+	/////////////////////////////
 
 	@Override
 	public void adLoadingCompleted(SASAdElement adElement) {
 		MoPubExtension.log("SAS banner loaded.");
-		if (banner != null) {
+		if(banner != null) {
 			banner.setOnClickListener(this);
 			banner.executeOnUIThread(new Runnable() {
-				@Override
-				public void run() {
-					if (banner != null)
+				@Override public void run() {
+					if(banner != null)
 						banner.setVisibility(View.VISIBLE);
-					if (listener != null && banner != null)
+					if(listener != null && banner != null)
 						listener.onBannerLoaded(banner);
 				}
 			});
@@ -97,98 +92,60 @@ public class SmartAdBanner extends CustomEventBanner implements View.OnClickList
 
 	@Override
 	public void adLoadingFailed(Exception e) {
-		final MoPubErrorCode errorCode = SmartAdUtils
-				.getMopubErrorCodeFromSmartAdException(e);
+		final MoPubErrorCode errorCode = SmartAdUtils.getMopubErrorCodeFromSmartAdException(e);
 		MoPubExtension.logW("SAS banner failed to load : " + e);
-		if (banner != null) {
+		if(banner != null) {
 			banner.executeOnUIThread(new Runnable() {
-				@Override
-				public void run() {
-					if (banner != null)
+				@Override public void run() {
+					if(banner != null)
 						banner.setVisibility(View.GONE);
-					if (listener != null)
+					if(listener != null)
 						listener.onBannerFailed(errorCode);
 				}
 			});
-		} else if (listener != null)
+		}
+		else if(listener != null)
 			listener.onBannerFailed(errorCode);
 	}
 
-	// /////////////////////
-	// LOCATION HANDLING //
-	// /////////////////////
 
-	private void setupLocationInfo(Context context) {
-		final LocationListener mLocationListener = new LocationListener() {
-			@Override public void onStatusChanged(String arg0, int arg1, Bundle arg2) { }
-			@Override public void onProviderEnabled(String arg0) { }
-			@Override public void onProviderDisabled(String arg0) { }
-
-			@Override
-			public void onLocationChanged(Location location) {
-				if (banner != null)
-					banner.setLocation(location);
-			}
-		};
-
-		final LocationManager mLocationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-		stateChangeListener = new SASAdView.OnStateChangeListener() {
-					@Override
-					public void onStateChanged(StateChangeEvent event) {
-						if (event.getType() == StateChangeEvent.VIEW_EXPANDED) {
-							// we request location updates each 10s to the
-							// location manager and register our listener
-							mLocationManager.requestLocationUpdates(
-									LocationManager.NETWORK_PROVIDER, 10000, 0,
-									mLocationListener);
-						} else if (event.getType() == StateChangeEvent.VIEW_HIDDEN) {
-							// we stop listening to location updates as the
-							// banner is closed
-							mLocationManager.removeUpdates(mLocationListener);
-						}
-					}
-				};
-			banner.addStateChangeListener(stateChangeListener);
-	}
-
-	// ///////////////////////////////
+	/////////////////////////////////
 	// SAS AD STATE CHANGE METHODS //
-	// ///////////////////////////////
+	/////////////////////////////////
 
 	@Override
 	public void onStateChanged(StateChangeEvent event) {
-		if (banner == null || listener == null)
+		if(banner == null || listener == null)
 			return;
 
-		if (event.getType() == StateChangeEvent.VIEW_EXPANDED) {
+		if(event.getType() == StateChangeEvent.VIEW_EXPANDED) {
 			MoPubExtension.log("SAS banner expanded.");
 			banner.executeOnUIThread(new Runnable() {
-				@Override
-				public void run() {
+				@Override public void run() {
 					listener.onBannerExpanded();
 				}
 			});
 		}
 
-		else if (event.getType() == StateChangeEvent.VIEW_DEFAULT) {
+		else if(event.getType() == StateChangeEvent.VIEW_DEFAULT) {
 			MoPubExtension.log("SAS banner collapsed.");
 			banner.executeOnUIThread(new Runnable() {
-				@Override
-				public void run() {
+				@Override public void run() {
 					listener.onBannerCollapsed();
 				}
 			});
 		}
 	}
 
-	// //////////////////
+
+	////////////////////
 	// CLICK LISTENER //
-	// //////////////////
+	////////////////////
 
 	@Override
 	public void onClick(View v) {
 		MoPubExtension.log("SAS banner clicked.");
-		if (listener != null)
+		if(listener != null)
 			listener.onBannerClicked();
 	}
 }
