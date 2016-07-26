20dfc0259e8b8d205f017aef9ba6a934189101d5
Maxime Philipp <maxime@studiopixmix.com>
2016-07-26 16:22:23 +0200

    SM-24 : Added the iOS SDK. Created the adapters files (copying the inMobi ones for the moment)

---
+++ ios/MoPubIosExtension/Opera/OperaBannerCustomEvent.h
@@ -0,0 +1,17 @@
+//
+//  InMobiBannerCustomEvent.h
+//  MoPub
+//
+//  Copyright (c) 2013 MoPub, Inc. All rights reserved.
+//
+
+#import "MPBannerCustomEvent.h"
+#import "AdMarvelDelegate.h"
+
+/*
+ * Certified with version 3.5.0 of the AdMarvel SDK.
+ */
+
+@interface OperaBannerCustomEvent : MPBannerCustomEvent <AdMarvelDelegate>
+
+@end
